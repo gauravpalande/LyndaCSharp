0fbfa3cfc3b9a91aa91bf181b8f81d015fea9695
Gaurav Palande <gaurav.palande147@gmail.com>
2017-05-17 19:18:13 -0700

    Lynda tutorial exercise to study C# features: Variable type and value and Ouput the value

---
+++ LyndaCSharp1/LyndaCSharp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LyndaCSharp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define a variable with a value
+            float myNumber = 25.50f;
+
+            // Send the value of the variable to the console
+            Console.WriteLine(myNumber);
+        }
+    }
+}
